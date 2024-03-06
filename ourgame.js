44ce9e60b7f653a0c41deade5388bf3b55f30499
aalali <ahmedalali5294@gmail.com>
2024-03-06 22:04:56 +0300

    added route

--- ourgame.js
+++ ourgame.js
@@ -1,22 +1,52 @@
 const grid = document.querySelector('.grid')
 const width = 30
-let borders = [1, 2]
-
-
+let facing = "right"
+let currentPacmanPos = 673
+let borders = [31,32,33,34,35,36,37,38,39,40,42,44,45,46,47,
+    48,49,50,51,52,53,54,55,61,66, 96, 126, 156, 186, 216,
+     246, 276, 306, 336, 366, 396, 426, 456, 486, 516, 546,
+      576, 606, 636, 666, 696, 726, 756, 151, 152, 153, 154,
+       155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165,
+        166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 50,
+         80, 110, 140, 170, 200, 230, 260, 290, 320, 350, 380,
+          410, 440, 470, 500, 530, 560, 590, 620, 650, 680, 710,
+           740, 770,72,102,132,162,135,105,75,55,85,115,145,175,
+           205,235,265,189,219,249,250,251,252,255,256,257,261,315,312,
+           262,263,264,265,282,285,397,398,399,340,341,342,343,344,
+           345,346,347,348,339, 369, 399, 429, 459, 489, 519, 549,
+         579, 519, 520, 521, 522, 523, 524, 525, 526, 527, 348, 378,
+          408, 438, 468, 498, 528, 558, 588,409,410,411,412,394,395
+        ,571, 572, 573, 574, 575, 576, 577, 578, 579, 580, 581, 582,
+          585, 586, 587, 588, 589, 590, 591, 592, 593, 594, 595,
+          601,631,661, 612,642,672,615,645,675,666, 667, 668, 669,
+           670, 671, 672, 673, 674, 675, 676, 677, 678, 679, 680,662,663
+           ,683,684,685,663,693,723,753,713,743,773,751, 752, 753, 754,
+            755, 756,  759, 760, 761, 762, 764, 765, 766, 767, 768, 770,
+             771, 772, 773, 774,781,811,841,792,852,795,825,775,805,835,
+             865,841, 842, 843, 844, 845, 846, 847, 848, 849, 850, 851,
+         852, 853, 854, 855, 856, 857, 858,625, 655,181,211,241,242,243,244,245,699,729,708,738,41,91,121,197,227, 859, 860, 861, 862, 863, 864, 822]
 for (let i=0; i<width*width;i++){
    const square = document.createElement('div')
    square.setAttribute("id", i)
    grid.appendChild(square)
 }
-for (let i=0;i<width;i++){
-    borders.push(i)
+const squares = Array.from(document.querySelectorAll('.grid div'))
+
+function createroute(){
+for (i=0;i<borders.length;i++){
+    squares[borders[i]].classList.add('bread')
 }
-for (let i=width;i<width*width;){
-    borders.push(i)
-    i += width
+}createroute()
+
+pacmanPos(currentPacmanPos)
+pacmanPos(currentPacmanPos+1)
+function pacmanPos(pos){
+    squares[currentPacmanPos].classList.remove('pacman')
+    squares[pos].classList.add('pacman')
+    currentPacmanPos = pos
 }
-for (let i=59;i<width*width;){
-    borders.push(i)
-    i += width
-}
-console.log(borders)
+// function movePacman(e) {
+//     switch(e.key){
+//         case 'ArrowLeft':
+//     }
+// }
